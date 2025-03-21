52263707c43a7a386d922000885645bae0db035b
emilainoPiz <emilianopizzuti95@gmail.com>
2025-03-21 19:01:47 +0100

    improve

--- main.js
+++ main.js
@@ -478,17 +478,6 @@ const data = {
       contactInfo.style.display === "flex" ? "none" : "flex";
   });
   
-  document.querySelector('.theme-switch-input').addEventListener('change', () => {
-    document.body.classList.toggle('dark-theme');
-  
-    // Force iOS Safari to repaint without removing the body from layout:
-    document.body.style.transform = 'none';
-    // Accessing offsetWidth or offsetHeight triggers the reflow
-    void document.body.offsetWidth;
-    // Revert the transform
-    document.body.style.transform = '';
-  });
-  
   
   /* ============================================================
      // Optional: Particles.js Configuration (Commented Out)
